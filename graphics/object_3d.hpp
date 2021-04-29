9e293b9f1595d7c64f42d8bcae09c2a4001353ba
Matt L <primeMuon@gmail.com>
2021-04-29 22:57:52 +0100

    Add object_2d and _3d

---
+++ graphics/object_3d.hpp
@@ -0,0 +1,30 @@
+#ifndef OBJECT_3D_HPP
+#define OBJECT_3D_HPP
+
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include "object_2d.hpp"
+
+using namespace std;
+using namespace sf;
+
+class Object3D
+{
+public:
+	Object3D();
+	~Object3D();
+
+private:
+};
+
+Object3D::Object3D()
+{
+	
+}
+
+Object3D::~Object3D()
+{
+
+}
+
+#endif
