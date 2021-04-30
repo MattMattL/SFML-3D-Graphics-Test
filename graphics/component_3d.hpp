248dc907157e21390f3bc0793f2661c7fe48fc1d
Matt L <primeMuon@gmail.com>
2021-04-30 06:12:56 +0100

    Replace object_2d with component_3d.hpp

---
+++ graphics/component_3d.hpp
@@ -0,0 +1,74 @@
+#ifndef COMPONENT_3D_HPP
+#define COMPONENT_3D_HPP
+
+#include <SFML/Graphics.hpp>
+#include <vector>
+
+using namespace std;
+using namespace sf;
+
+class Component3D
+{
+public:
+	Component3D();
+	~Component3D();
+
+	Vector3<float> at(int);
+	int size();
+
+	void add(float, float, float);
+	void add(Vector3<float>);
+
+	Component3D translate(float, float, float);
+
+private:
+	vector<Vector3<float> > points;
+};
+
+Component3D::Component3D()
+{
+	
+}
+
+Component3D::~Component3D()
+{
+
+}
+
+Vector3<float> Component3D::at(int index)
+{
+	return points.at(index);
+}
+
+int Component3D::size()
+{
+	return points.size();
+}
+
+void Component3D::add(float x, float y, float z)
+{
+	Vector3<float> vec;
+
+	vec.x = x;
+	vec.y = y;
+	vec.z = z;
+
+	points.push_back(vec);
+}
+
+void Component3D::add(Vector3<float> vec)
+{
+	points.push_back(vec);
+}
+
+Component3D Component3D::translate(float deltaX, float deltaY, float deltaZ)
+{
+	Component3D component;
+
+	for(int i=0; i<points.size(); i++)
+		component.add(points.at(i).x + deltaX, points.at(i).y + deltaY, points.at(i).z + deltaZ);
+
+	return component;
+}
+
+#endif
--- graphics/object_3d.hpp
+++ graphics/object_3d.hpp
@@ -3,7 +3,7 @@
 
 #include <SFML/Graphics.hpp>
 #include <vector>
-#include "object_2d.hpp"
+#include "component_3d.hpp"
 
 using namespace std;
 using namespace sf;
@@ -14,7 +14,13 @@ public:
 	Object3D();
 	~Object3D();
 
+	Component3D at(int);
+	int size();
+
+	void add(Component3D);
+
 private:
+	vector<Component3D> components;
 };
 
 Object3D::Object3D()
@@ -26,5 +32,21 @@ Object3D::~Object3D()
 {
 
 }
+
+Component3D Object3D::at(int index)
+{
+	return components.at(index);
+}
+
+int Object3D::size()
+{
+	return components.size();
+}
+
+void Object3D::add(Component3D component)
+{
+	components.push_back(component);
+}
+
 
 #endif
